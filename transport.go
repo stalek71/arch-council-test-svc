a8870ded6c31e114eb14f0b75ad0c275813041da
Alek Stankiewicz <stankiewiczal@gmail.com>
2020-04-16 13:08:08 +0200

    Update transport.go

--- transport.go
+++ transport.go
@@ -16,7 +16,7 @@ func makeUppercaseEndpoint(svc StringService) endpoint.Endpoint {
 		req := request.(uppercaseRequest)
 		v, err := svc.Uppercase(req.S)
 		hostName, _ := os.Hostname()
-		version := "0.1.2"
+		version := "0.1.3"
 		if err != nil {
 			return uppercaseResponse{v, hostName, version, err.Error()}, nil
 		}
